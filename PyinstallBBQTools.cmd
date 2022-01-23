f6f76f8d76f6735154f593a6e8aea2f6bad8c86e
swrine <swrine@gmail.com>
2022-01-23 17:09:52 +0800

    Add company logo

--- PyinstallBBQTools.cmd
+++ PyinstallBBQTools.cmd
@@ -10,6 +10,6 @@ pyinstaller --noconsole --icon=resources/BBQTools.ico BBQTools.py
 mkdir %pyinstaller_dist_path%\resources
 xcopy /s /y /i "resources" %pyinstaller_dist_path%\resources
 xcopy /s /y /i "oraclientdlls" %pyinstaller_dist_path%
-for %%F in (config.yaml trmasterdata.json cfmasterdata.csv RunBBQToolsWithPath.cmd) do xcopy /y /i %%F %pyinstaller_dist_path%
+for %%F in (*config.yaml *trmasterdata.json *cfmasterdata.csv RunBBQToolsWithPath.cmd) do xcopy /y /i %%F %pyinstaller_dist_path%
 
 @echo on
